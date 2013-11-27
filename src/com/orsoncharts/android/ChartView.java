d1780f88235ed4f5dcc186476ad0e9791e1dc356
David Gilbert <david.gilbert@object-refinery.com>
2013-11-27 08:31:23 +0100

    Add zoomToFit() and remove hard-coded demo.

--- src/com/orsoncharts/android/ChartView.java
+++ src/com/orsoncharts/android/ChartView.java
@@ -8,23 +8,29 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.View.OnTouchListener;
 
-import com.orsoncharts.android.data.DefaultKeyedValues;
-import com.orsoncharts.android.data.StandardPieDataset3D;
-import com.orsoncharts.android.data.category.CategoryDataset3D;
-import com.orsoncharts.android.data.category.StandardCategoryDataset3D;
+import com.orsoncharts.android.demo.PieChartDemo1;
+import com.orsoncharts.android.graphics3d.Dimension2D;
+import com.orsoncharts.android.graphics3d.Dimension3D;
 import com.orsoncharts.android.graphics3d.ViewPoint3D;
 
+/**
+ * A custom view for displaying a chart.
+ */
 public class ChartView extends View implements Chart3DChangeListener {
 
+	/** The chart. */
+	private Chart3D chart;
+
+	/** The paint-brush to use while drawing the chart. */
 	private Paint paint;
 	
-	private Chart3D chart;
-	
+	/** The chart bounds (reusable object). */
 	private RectF bounds;
 	
 	private ScaleGestureDetector scaleGestureDetector;
+	
+	private float margin = 0.15f;
 	
 	public ChartView(Context context) {
 		super(context);
@@ -44,114 +50,38 @@ public class ChartView extends View implements Chart3DChangeListener {
 	private void init(Context context) {
 		this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		this.bounds = new RectF();
-		this.chart = createChart();
+		this.chart = PieChartDemo1.createChart();
 		this.chart.getViewPoint().setRho(20);
 		this.chart.addChangeListener(this);
 		scaleGestureDetector = new ScaleGestureDetector(context,
 		        new ScaleListener());		
 	}
 	
-	private Chart3D createChart() {
-		StandardPieDataset3D dataset = new StandardPieDataset3D();
-		dataset.add("A", 20);
-		dataset.add("B", 30);
-		dataset.add("C", 40);
-		return Chart3DFactory.createPieChart("title", "subtitle", dataset);
-	}
-	
-	private Chart3D createChart2() {
-		return Chart3DFactory.createBarChart("Title", "Subtitle", 
-				createDataset2(), "Row Axis", "Column Axis", "Value Axis");
+	public void setChart(Chart3D chart) {
+		this.chart = chart;
+		Dimension2D size = new Dimension2D(getWidth(), getHeight());
+		zoomToFit(size);
+		invalidate();
 	}
 	
     /**
-     * Creates a sample dataset (hard-coded for the purpose of keeping the
-     * demo self-contained - in practice you would normally read your data
-     * from a file, database or other source).
+     * Adjusts the viewing distance so that the chart fits the specified
+     * size.  A margin is left (see {@link #getMargin()} around the edges to 
+     * leave room for labels etc.
      * 
-     * @return A sample dataset.
-     */
-    private static CategoryDataset3D createDataset2() {    
-        StandardCategoryDataset3D dataset = new StandardCategoryDataset3D();
-                
-        DefaultKeyedValues s3 = new DefaultKeyedValues();
-        s3.put("Jan", 7);
-        s3.put("Feb", 7);
-        s3.put("Mar", 10);
-        s3.put("Apr", 13);
-        s3.put("May", 17);
-        s3.put("Jun", 20);
-        s3.put("Jul", 22);
-        s3.put("Aug", 21);
-        s3.put("Sep", 19);
-        s3.put("Oct", 15);
-        s3.put("Nov", 10);
-        s3.put("Dec", 8);
-        dataset.addSeriesAsRow("London", s3);
-
-        DefaultKeyedValues s1 = new DefaultKeyedValues();
-        s1.put("Jan", 3);
-        s1.put("Feb", 5);
-        s1.put("Mar", 9);
-        s1.put("Apr", 14);
-        s1.put("May", 18);
-        s1.put("Jun", 22);
-        s1.put("Jul", 25);
-        s1.put("Aug", 24);
-        s1.put("Sep", 20);
-        s1.put("Oct", 14);
-        s1.put("Nov", 8);
-        s1.put("Dec", 4);
-        dataset.addSeriesAsRow("Geneva", s1);
-        
-        DefaultKeyedValues s2 = new DefaultKeyedValues();
-        s2.put("Jan", 9);
-        s2.put("Feb", 11);
-        s2.put("Mar", 13);
-        s2.put("Apr", 16);
-        s2.put("May", 20);
-        s2.put("Jun", 23);
-        s2.put("Jul", 26);
-        s2.put("Aug", 26);
-        s2.put("Sep", 24);
-        s2.put("Oct", 19);
-        s2.put("Nov", 13);
-        s2.put("Dec", 9);
-        dataset.addSeriesAsRow("Bergerac", s2);
-
-        DefaultKeyedValues s4 = new DefaultKeyedValues();
-        s4.put("Jan", 22);
-        s4.put("Feb", 22);
-        s4.put("Mar", 20);
-        s4.put("Apr", 17);
-        s4.put("May", 14);
-        s4.put("Jun", 11);
-        s4.put("Jul", 11);
-        s4.put("Aug", 12);
-        s4.put("Sep", 14);
-        s4.put("Oct", 17);
-        s4.put("Nov", 19);
-        s4.put("Dec", 21);
-        dataset.addSeriesAsRow("Christchurch", s4);
-
-        DefaultKeyedValues s5 = new DefaultKeyedValues();
-        s5.put("Jan", 20);
-        s5.put("Feb", 20);
-        s5.put("Mar", 19);
-        s5.put("Apr", 17);
-        s5.put("May", 14);
-        s5.put("Jun", 12);
-        s5.put("Jul", 11);
-        s5.put("Aug", 12);
-        s5.put("Sep", 13);
-        s5.put("Oct", 15);
-        s5.put("Nov", 17);
-        s5.put("Dec", 19);
-        dataset.addSeriesAsRow("Wellington", s5);
-
-        return dataset;
+     * @param size  the target size (<code>null</code> not permitted).
+     */    
+    public void zoomToFit(Dimension2D size) {
+        int w = (int) (size.getWidth() * (1.0 - this.margin));
+        int h = (int) (size.getHeight() * (1.0 - this.margin));
+        Dimension2D target = new Dimension2D(w, h);
+        Dimension3D d3d = this.chart.getDimensions();
+        float distance = this.chart.getViewPoint().optimalDistance(target, 
+                d3d);
+        this.chart.getViewPoint().setRho(distance);
+        invalidate();        
     }
-
+    
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		int measuredWidth = measureWidth(widthMeasureSpec);
@@ -177,8 +107,10 @@ public class ChartView extends View implements Chart3DChangeListener {
 		int height = getMeasuredHeight();
 		int width = getMeasuredWidth();
 		this.bounds.set(0f, 0f, width, height);
-		chart.draw(canvas, paint, bounds);
-	}
+		if (chart != null) {
+            chart.draw(canvas, paint, bounds);
+		}
+    }
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event)  {
